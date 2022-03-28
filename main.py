5b55ce5e0889528eee86593cdb7975bd24be1e15
Terry Drymonacos <terrdry@gmail.com>
2022-03-27 23:57:47 -0700

    Penultimate version; next is documentation and adding some basic unit tests --continued

--- main.py
+++ main.py
@@ -33,52 +33,57 @@ class OrgTodoParser():
     initRegExpr = r'^\*\sTODO\s.*'
     dateRegExpr = r'^SCHEDULED:\s<(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+)\s(?P<dom>\w*)'
 
-    def __init__(self, input_directory, postdays, output_file):
-        self.input_directory = input_directory
+    def __init__(self, input_dir, postdays, output_file):
+        self.input_dir = input_dir
         self.postdays = postdays
         self.output_file = output_file
-        self.todo_list_toprint = None
+        self.list_to_print = list()
 
-    def parseTodo(self):
+    def prepare_parsing(self):
         init_expr = re.compile(self.initRegExpr)
         todo_expr = re.compile(self.todoRegExpr, re.MULTILINE)
 
-        file_list = list(filter(lambda x: x.endswith('.org'), os.listdir(self.input_directory)))
-        self.grab_todo(file_list, self.input_directory, init_expr, todo_expr)
+        file_list = list(filter(lambda x: x.endswith('.org'), os.listdir(self.input_dir)))
+        self.process_todo(file_list, self.input_dir, init_expr, todo_expr)
 
-    def grab_todo(self, file_list, file_directory, init_expr, todo_expr):
-        self.todo_list_toprint = list()
+    def process_todo(self, file_list, file_directory, init_expr, todo_expr):
         for elem in file_list:
             with open(file_directory + elem, 'r') as fh:
-                lines = fh.readlines()
-                for k, item in enumerate(lines):
-                    todo_match = re.search(init_expr, item)
-                    found = False
-                    if todo_match:
-                        found = True
-                        matches = re.finditer(todo_expr, item)
-                        our_tags = 0
-                        for matchNum, theMatch in enumerate(matches):
-                            for group_num in range(0, len(theMatch.groups())):
-                                if group_num == 1:
-                                    todo_message = theMatch.groupdict()['text'].strip('\n')
-                                if group_num == 3:
-                                    our_tags = theMatch.groupdict()['tags']
-                            self.todo_list_toprint.append(dict({'todo_message': todo_message,
-                                                                'tags': our_tags,
-                                                                'date': ""
-                                                                }
-                                                               )
-                                                          )
-                    if found:
-                        self.grab_date(k, lines)
+                self.read_file(fh, init_expr, todo_expr)
+
         # process the list now and filter out the dates
-        self.filter_date()
+        self.filter_dates()
 
         # Print out our result array
+        self.print_to_file()
+
+    def print_to_file(self):
         with open(self.output_file, "w") as fh:
-            foo = [self.to_org_format(elem) for elem in self.todo_list_toprint]
-            fh.writelines([self.to_org_format(elem) for elem in self.todo_list_toprint])
+            fh.writelines([self.to_org_format(elem) for elem in self.list_to_print])
+
+    def read_file(self, fh, init_expr, todo_expr):
+        lines = fh.readlines()
+        for k, item in enumerate(lines):
+            todo_match = re.search(init_expr, item)
+            found = False
+            if todo_match:
+                found = True
+                matches = re.finditer(todo_expr, item)
+                our_tags = 0
+                for matchNum, theMatch in enumerate(matches):
+                    for group_num in range(0, len(theMatch.groups())):
+                        if group_num == 1:
+                            todo_message = theMatch.groupdict()['text'].strip('\n')
+                        if group_num == 3:
+                            our_tags = theMatch.groupdict()['tags']
+                    self.list_to_print.append(dict({'todo_message': todo_message,
+                                                        'tags': our_tags,
+                                                        'date': ""
+                                                    }
+                                                   )
+                                              )
+            if found:
+               self.grab_date(k, lines)
 
     def grab_date(self, k, lines):
         # let's see if the next record has our Scheduled Date
@@ -91,19 +96,19 @@ class OrgTodoParser():
                                           int(date_line.groupdict()["month"]),
                                           int(date_line.groupdict()["day"])
                                           )
-                self.todo_list_toprint[-1]['date'] = this_date
+                self.list_to_print[-1]['date'] = this_date
 
 
-    def filter_date(self):
-        for k in range(len(self.todo_list_toprint)-1, -1, -1):
-            if self.todo_list_toprint[k]['date']:
-                this_date = self.todo_list_toprint[k]['date']
+    def filter_dates(self):
+        for k in range(len(self.list_to_print) - 1, -1, -1):
+            if self.list_to_print[k]['date']:
+                this_date = self.list_to_print[k]['date']
                 now = datetime.date.today()
                 time_delta = this_date - now
                 if not( time_delta.days <= self.postdays):
-                    del self.todo_list_toprint[k]
+                    del self.list_to_print[k]
             else:
-                del self.todo_list_toprint[k]
+                del self.list_to_print[k]
 
     def to_org_format(self, date_element):
         return " - [ ] #todo {}  <span class='cm-strong'>{}</span>\n".format(date_element['todo_message'], date_element['date'])
@@ -111,7 +116,7 @@ class OrgTodoParser():
 
 def main(input_directory, postdays, output_file):
     a = OrgTodoParser(input_directory, postdays, output_file)
-    a.parseTodo()
+    a.prepare_parsing()
 
 
 if __name__ == "__main__":
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,7 @@
 import unittest
-from main import create_argparser
+import datetime
+import mock
+from main import create_argparser, OrgTodoParser
 
 class OptionParser(unittest.TestCase):
     srcdir = "/home/terryd/todofiles/"
@@ -27,3 +29,103 @@ class OptionParser(unittest.TestCase):
 
 
 
+class todoParser(unittest.TestCase):
+
+    def test_constructor_init(self):
+        input_dir = "/home/someone/"
+        postdays = 2
+        output_file = "/home/orgzly/"
+        todo_parser = OrgTodoParser(input_dir, postdays, output_file)
+        self.assertEqual(todo_parser.input_dir, input_dir)
+        self.assertEqual(todo_parser.postdays, postdays)
+        self.assertEqual(todo_parser.output_file, output_file)
+
+    def test_org_format(self):
+        input_dir = "/home/someone/"
+        postdays = 2
+        output_file = "/home/orgzly/"
+
+        test_date_element = {'todo_message': "do this",
+                             'date': "2022-03-14"
+                             }
+        todo_parser = OrgTodoParser(input_dir, postdays, output_file)
+        self.assertEqual(todo_parser.to_org_format(test_date_element),
+                         " - [ ] #todo do this  <span class='cm-strong'>2022-03-14</span>\n")
+
+    def test_filter_dates(self):
+
+        todo_parser = OrgTodoParser("", 0, "")
+        the_past = datetime.timedelta(days=-100)
+        the_future = datetime.timedelta(days=100)
+        todo_parser.list_to_print = [
+            {
+                'todo_message': "do this yesterday",
+                'date': datetime.date.today() + the_past
+            },
+            {
+                'todo_message': "this is not scheduled",
+                'date': ''
+            },
+            {
+                'todo_message': "do this today",
+                'date': datetime.date.today()
+            },
+            {
+                'todo_message': "do this in the future",
+                'date': datetime.date.today() + the_future
+            },
+        ]
+        todo_parser.filter_dates()
+        self.assertEqual(len(todo_parser.list_to_print), 2)
+
+    def test_filter_dates_future(self):
+
+        todo_parser = OrgTodoParser("", 0, "")
+        the_future = datetime.timedelta(days=100)
+        the_penultimate = datetime.timedelta(days=200)
+        the_infinity = datetime.timedelta(days=300)
+        todo_parser.list_to_print = [
+            {
+                'todo_message': "do this in a 100 days",
+                'date': datetime.date.today() + the_future
+            },
+            {
+                'todo_message': "do this in 200 days",
+                'date': datetime.date.today() + the_penultimate
+            },
+            {
+                'todo_message': "this is not scheduled",
+                'date': ''
+            },
+            {
+                'todo_message': "do this in 300 days ",
+                'date': datetime.date.today() + the_infinity
+            },
+        ]
+        todo_parser.filter_dates()
+        self.assertEqual(len(todo_parser.list_to_print), 0)
+    def test_filter_dates_onlytoday(self):
+
+        todo_parser = OrgTodoParser("", 0, "")
+        the_penultimate = datetime.timedelta(days=200)
+        the_infinity = datetime.timedelta(days=300)
+        todo_parser.list_to_print = [
+            {
+                'todo_message': "do this today",
+                'date': datetime.date.today()
+            },
+            {
+                'todo_message': "this is not scheduled",
+                'date': ''
+            },
+            {
+                'todo_message': "do this in 200 days",
+                'date': datetime.date.today() + the_penultimate
+            },
+            {
+                'todo_message': "do this in 300 days ",
+                'date': datetime.date.today() + the_infinity
+            },
+        ]
+        todo_parser.filter_dates()
+        self.assertEqual(len(todo_parser.list_to_print), 1)
